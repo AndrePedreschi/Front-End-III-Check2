b4f5131088548f975b46d23b9bc08ba793aa31aa
AndrePedreschi <a.pedreschi32@gmail.com>
2022-12-08 00:21:43 -0300

    Ajustes Rotas

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,55 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import Navbar from "./Components/Navbar";
-import Home from "./Routes/Home";
-import Footer from "./Components/Footer";
+import  Home  from "./Routes/Home";
+import  Login  from "./Routes/Login";
+import  Detail  from "./Routes/Detail";
 import "./index.css";
+import  App  from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "./hooks/useTheme"
+import { Navigate } from "react-router-dom";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //Lembre-se de configurar suas rotas e seu contexto aqui
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Navigate to='home' />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "Login",
+        element: <Login />,
+      },
+      {
+        path: "detail/:id",
+        element: <Detail />,
+      },
+    ]
+  }
+])
+
+
+
+
+
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <Home />
-    <Footer />
+    <ThemeProvider>
+    
+      <RouterProvider router={router} />
+
+    </ThemeProvider>
   </React.StrictMode>
 );
