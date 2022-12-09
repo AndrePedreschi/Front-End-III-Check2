3ce65f9272fe49d63944d70ff58ddf08d368d0d5
camila <cftinelli@outlook.com>
2022-12-09 17:28:55 -0300

    iniciando o login

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,7 +1,15 @@
 import styles from "./Form.module.css";
+import { useState } from "react";
 
 const LoginForm = () => {
+
+  const [loginUser, setLoginUser] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit", { loginUser, password });
+
     //Nesse handlesubmit você deverá usar o preventDefault,
     //enviar os dados do formulário e enviá-los no corpo da requisição 
     //para a rota da api que faz o login /auth
@@ -10,6 +18,10 @@ const LoginForm = () => {
     //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
     //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erro
   };
+
+  function loginSucess() {
+
+  }
 
   return (
     <>
@@ -23,8 +35,10 @@ const LoginForm = () => {
             <input
               className={`form-control ${styles.inputSpacing}`}
               placeholder="Login"
-              name="login"
+              name="loginUser"
+              type="text"
               required
+              value={loginUser} onChange={(e) => setLoginUser(e.target.value)}
             />
             <input
               className={`form-control ${styles.inputSpacing}`}
@@ -32,9 +46,11 @@ const LoginForm = () => {
               name="password"
               type="password"
               required
+              value={password} onChange={(e) => setPassword(e.target.value)}
+
             />
-            <button className="btn btn-primary" type="submit">
-              Send
+            <button onClick={loginSucess} className="btn btn-primary" type="submit">
+              Enviar
             </button>
           </form>
         </div>
